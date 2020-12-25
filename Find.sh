6e49cf1cc093acb88c5617ca437cfc39a082c805
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-12-26 01:21:35 +0530

    Update Find.sh

--- Find.sh
+++ Find.sh
@@ -13,7 +13,7 @@ echo "N[7] telegram group"
 echo "N[8] exit"                                               
 echo ""
 echo -e "$p"
-read -p  "number-------> " N
+read -p  "choose-number-> " N
 
 if [ "$N" -eq "1"  ]; then
  cd
@@ -21,25 +21,25 @@ if [ "$N" -eq "1"  ]; then
  termux-setup-storage
  clear
  echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
+ sleep 0.04
  clear 
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
+ sleep 0.04
  cd
  git clone https://github.com/ShuBhamg0sain/mywhagp
  cd mywhagp
  chmod +x * wa1.py
  clear
- sleep 2 
+ sleep 1 
  python2 wa1.py
 echo "    {Ctrl C} exit "
- sleep 2
+ sleep 1
  cd
  git clone https://github.com/ShuBhamg0sain/FIND-ME
  cd FIND-ME
@@ -53,25 +53,25 @@ elif [ "$N" -eq "2"  ]; then
  termux-setup-storage
  clear
  echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
+ sleep 0.04
  clear 
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
+ sleep 0.04
  cd
  git clone https://github.com/ShuBhamg0sain/mywhagp
  cd mywhagp
  chmod +x * wa2.py
  clear
- sleep 2 
+ sleep 1 
  python2 wa2.py
 echo "    {Ctrl C} exit "
- sleep 2
+ sleep 1
  cd
  git clone https://github.com/ShuBhamg0sain/FIND-ME
  cd FIND-ME
@@ -84,25 +84,25 @@ elif [ "$N" -eq "3"  ]; then
  termux-setup-storage
  clear
  echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
+ sleep 0.04
  clear 
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
+ sleep 0.04
  cd
  git clone https://github.com/ShuBhamg0sain/mywhagp
  cd mywhagp
  chmod +x * wa3.py
  clear
- sleep 2 
+ sleep 1 
  python2 wa3.py
 echo "    {Ctrl C} exit "
- sleep 2
+ sleep 1
  cd
  git clone https://github.com/ShuBhamg0sain/FIND-ME
  cd FIND-ME
@@ -116,25 +116,25 @@ elif [ "$N" -eq "4"  ]; then
  termux-setup-storage
  clear
  echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
+ sleep 0.04
  clear 
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
+ sleep 0.04
  cd
  git clone https://github.com/ShuBhamg0sain/mywhagp
  cd mywhagp
  chmod +x * wa4.py
  clear
- sleep 2 
+ sleep 1 
  python2 wa4.py
 echo "    {Ctrl C} exit "
- sleep 2
+ sleep 1
  cd
  git clone https://github.com/ShuBhamg0sain/FIND-ME
  cd FIND-ME
@@ -148,25 +148,25 @@ elif [ "$N" -eq "5"  ]; then
  termux-setup-storage
  clear
  echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
+ sleep 0.04
  clear 
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
+ sleep 0.04
  cd
  git clone https://github.com/ShuBhamg0sain/myinsta
  cd myinsta
  chmod +x * insta.py
  clear
- sleep 2 
+ sleep 1 
  python2 insta.py
 echo "    {Ctrl C} exit "
- sleep 2
+ sleep 1
  cd
  git clone https://github.com/ShuBhamg0sain/FIND-ME
  cd FIND-ME
@@ -180,25 +180,25 @@ elif [ "$N" -eq "6"  ]; then
  termux-setup-storage
  clear
  echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
+ sleep 0.04
  clear 
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
+ sleep 0.04
  cd
  git clone https://github.com/ShuBhamg0sain/myfacbook
  cd myfacbook
  chmod +x * face.py
  clear
- sleep 2 
+ sleep 1 
  python2 face.py
 echo "    {Ctrl C} exit "
- sleep 2
+ sleep 1
  cd
  git clone https://github.com/ShuBhamg0sain/FIND-ME
  cd FIND-ME
@@ -212,25 +212,25 @@ elif [ "$N" -eq "7"  ]; then
  termux-setup-storage
  clear
  echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
+ sleep 0.04
  clear 
  echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
- sleep 0.4
+ sleep 0.04
  cd
  git clone https://github.com/ShuBhamg0sain/mytelegp
  cd mytelegp
  chmod +x * tele.py
  clear
- sleep 2 
+ sleep 1 
  python2 tele.py
 echo "    {Ctrl C} exit "
- sleep 2
+ sleep 1
  cd
  git clone https://github.com/ShuBhamg0sain/FIND-ME
  cd FIND-ME
@@ -243,15 +243,18 @@ elif [ "$N" -eq "8"  ]; then
  termux-setup-storage
  clear
  echo -e $g 'Please Wait ===+['$p'>              '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
- sleep 0.4
+ sleep 0.04
  clear
  echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
- sleep 0.4
+ sleep 0.04
  clear 
  echo -e "$g+++++++++++++++>$p[thanks for finding me]$g<++++++++++++++"
- sleep 0.4
+ sleep 0.04
 echo "    {Ctrl C} exit "
+
+else :
+ bash Find.sh
 fi
